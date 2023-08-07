287846ee67c8e26c04a75294382bbbdc4e83b961
Matheus <matheusvmja@gmail.com>
2023-08-07 02:27:32 -0300

    Feat: GET/ranking finished

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -3,7 +3,9 @@ import { db } from "../database/database.js"
 export async function getRanking(req, res) {
     
     try {
+        const ranking = await db.query(`SELECT users.id, users.name, COUNT(urls."userId") AS "linksCount", SUM(urls."visitCount") AS "visitCount" FROM urls JOIN users ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;`)
 
+        return res.status(200).send(ranking.rows)
     } catch (err) {
         return res.status(500).send(err.message)
     }
--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,8 +4,8 @@ export async function getUser(req, res) {
     const { userId } = res.locals
 
     try {
-        const userProfileMain = await db.query(`SELECT users.id, users.name, SUM(urls."visitCount") AS "visitCount" FROM users JOIN urls ON users.id = urls."userId" WHERE users.id = $1 GROUP BY users.id`, [userId])
-        const userProfileUrls = await db.query(`SELECT urls.id, urls."shortUrl", urls.url, urls."visitCount" FROM urls WHERE urls."userId" = $1`, [userId])
+        const userProfileMain = await db.query(`SELECT users.id, users.name, SUM(urls."visitCount") AS "visitCount" FROM users JOIN urls ON users.id = urls."userId" WHERE users.id = $1 GROUP BY users.id;`, [userId])
+        const userProfileUrls = await db.query(`SELECT urls.id, urls."shortUrl", urls.url, urls."visitCount" FROM urls WHERE urls."userId" = $1;`, [userId])
         const userProfile = {...userProfileMain.rows[0], shortenedUrls: userProfileUrls.rows}
 
         res.status(200).send(userProfile)
